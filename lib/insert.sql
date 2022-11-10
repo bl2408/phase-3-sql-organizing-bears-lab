4da0d924f067e2c4450af84cfb681b7f72f6fb51
Brian Lambert <brianlambert2408@gmail.com>
2022-11-10 14:04:03 +1100

    Completed tasks

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 2, "M", "Brown", "Angry", 1),
+("Rowdy", 2, "M", "Brown", "Angry", 1),
+("Tabitha", 2, "F", "Brown", "Angry", 1),
+("Sergeant Brown", 2, "M", "Brown", "Angry", 1),
+("Melissa", 2, "F", "Brown", "Angry", 1),
+("Grinch", 2, "M", "Brown", "Angry", 1),
+("Wendy", 2, "F", "Brown", "Angry", 1),
+(null, 2, "M", "Brown", "Angry", 1);
